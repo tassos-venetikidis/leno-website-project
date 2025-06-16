0e7993715fd1bc18daeb60d2469695ea80b5db55
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-06-16 16:02:55 +0300

    Added the scroll functionality of the navbar

--- js/script.js
+++ js/script.js
@@ -9,3 +9,12 @@ document.addEventListener("DOMContentLoaded", function () {
     mobileMenuItems.classList.toggle("active");
   });
 });
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar--scroll");
+  } else {
+    navbar.classList.remove("navbar--scroll");
+  }
+});
