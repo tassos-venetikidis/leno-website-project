2121d2f95b1374159662f4ccc7a4f456772dacbf
Tassos Venetikidis <tassos.venetikidis@gmail.com>
2025-06-19 19:03:14 +0300

    Added the preview section along with the animated play button and modal

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
+  // Mobile Menu
   const toggleButton = document.querySelector(".navbar__mobile-menu-toggle");
 
   toggleButton.addEventListener("click", function () {
@@ -8,8 +9,39 @@ document.addEventListener("DOMContentLoaded", function () {
 
     mobileMenuItems.classList.toggle("active");
   });
+
+  // Video Modal
+  const modal = document.getElementById("videoModal");
+  const videoButton = document.querySelector(".preview__video-button");
+  const closeButton = document.querySelector(".modal__close-button");
+  const videoPlayer = document.getElementById("videoPlayer");
+
+  // Open Modal When Clicked
+  videoButton.addEventListener("click", function () {
+    // Show Modal
+    modal.setAttribute("style", "display: block;");
+
+    // Replace the src attribute with the video URL
+    videoPlayer.src =
+      "https://www.youtube.com/embed/TcLps_Ja51w?si=qyucWwbE9eZQU3WQ";
+
+    // Close Modal on Close Button Click
+    closeButton.addEventListener("click", function () {
+      modal.setAttribute("style", "display: none;");
+      videoPlayer.src = "";
+    });
+
+    // Close Modal on Outter Click
+    window.addEventListener("click", function (e) {
+      if (e.target === modal) {
+        modal.setAttribute("style", "display: none;");
+        videoPlayer.src = "";
+      }
+    });
+  });
 });
 
+// Navigation background on scroll
 window.addEventListener("scroll", function () {
   const navbar = document.querySelector(".navbar");
   if (window.scrollY > 0) {
